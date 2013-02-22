c29da088adaa94421637a065ae4c392dadb6924d
Ivan Ermilov <iermilov@informatik.uni-leipzig.de>
2013-02-22 13:15:41 +0100

    deleted plugins, corrected git-pull script

--- .vim/bundle/git-pull.sh
+++ .vim/bundle/git-pull.sh
@@ -4,15 +4,11 @@ git clone https://github.com/msanders/snipmate.vim.git ./snipmate
 git clone https://github.com/tpope/vim-surround.git ./surround
 git clone https://github.com/tpope/vim-git.git ./git
 git clone https://github.com/ervandew/supertab.git ./supertab
-git clone https://github.com/sontek/minibufexpl.vim.git ./minibufexpl
-git clone https://github.com/wincent/Command-T.git ./command-t
 git clone https://github.com/mitechie/pyflakes-pathogen.git
 git clone https://github.com/mileszs/ack.vim.git ./ack
 git clone https://github.com/sjl/gundo.vim.git ./gundo
-git clone https://github.com/fs111/pydoc.vim.git ./pydoc
 git clone https://github.com/vim-scripts/pep8.git ./pep8
 git clone https://github.com/alfredodeza/pytest.vim.git ./py.test
-git clone https://github.com/reinh/vim-makegreen ./makegreen
 git clone https://github.com/vim-scripts/TaskList.vim.git ./tasklist
 git clone https://github.com/vim-scripts/The-NERD-tree.git ./nerdtree
 git clone https://github.com/sontek/rope-vim.git ./ropevim
